459558ffddd8641364a3ce5e79f8853d0903cf37
ChepLj <nguyenvansy1992job02@gmail.com>
2022-07-16 11:57:07 +0700

    innit

---
+++ assets/script/main.js
@@ -0,0 +1,700 @@
+var dataMain = [
+    {
+    "id": "01",
+    "comment": "Group",
+    "description": "Khu Vực BF",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Area",
+    "description": "Nhà Quạt",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Equipment",
+    "description": "Tủ điện trung thế BPRT",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Relay trung thế báo lỗi",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": {
+    "220714": "Bình Thường"
+    }
+    }
+    ]
+    },
+    {
+    "id": "02",
+    "comment": "Equipment",
+    "description": "Motor BPRT (12MW)",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Nhiệt độ cuộn dây",
+    "type": [
+    "typing",
+    "A",
+    "B",
+    "C"
+    ],
+    "result": {
+    "220714": [
+    56.7,
+    66,
+    65
+    ]
+    }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": " Nhiệt độ bạc đạn",
+    "type": [
+    "typing",
+    "Đầu",
+    "Đuôi"
+    ],
+    "result": {
+    "220714": [
+    76,
+    55
+    ]
+    }
+    }
+    ]
+    },
+    {
+    "id": "03",
+    "comment": "Equipment",
+    "description": "Tủ điện trung thế D2500",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Relay trung thế báo lỗi",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "04",
+    "comment": "Equipment",
+    "description": "Motor D2500 (10.5MW)",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Nhiệt độ cuộn dây",
+    "type": [
+    "typing",
+    "A",
+    "B",
+    "C"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": " Nhiệt độ bạc đạn",
+    "type": [
+    "typing",
+    "Đầu",
+    "Đuôi"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "05",
+    "comment": "Equipment",
+    "description": "Tủ điện trung thế lộ phân phối",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Nhiệt độ (từ 25-32°C)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Độ ẩm trung bình (từ 65-75%)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    },
+    {
+    "id": "03",
+    "comment": "Content",
+    "description": "Relay trung thế báo lỗ",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "06",
+    "comment": "Equipment",
+    "description": "Hệ thống bù",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Tủ bù P1+P2",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Tủ bù E1+E2",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    },
+    {
+    "id": "03",
+    "comment": "Content",
+    "description": "Tủ bù F1+F2",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "07",
+    "comment": "Equipment",
+    "description": "MBA số 3",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Nhiệt độ",
+    "type": [
+    "typing",
+    "Dầu"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Âm thanh MBA",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "08",
+    "comment": "Equipment",
+    "description": "MBA số 4",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Nhiệt độ",
+    "type": [
+    "typing",
+    "Dầu"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Âm thanh MBA",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "09",
+    "comment": "Equipment",
+    "description": "Tủ UPS (BPRT+D2500)",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "( Ghi rõ cảnh báo nếu màn hình HMI báo)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "10",
+    "comment": "Equipment",
+    "description": "Tủ UPS (dãy phân phối)",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "( Ghi rõ cảnh báo nếu màn hình HMI báo)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "11",
+    "comment": "Equipment",
+    "description": "Tủ phân phối hạ thế",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "(Kiểm tra chi tiết từng ngăn tủ)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "12",
+    "comment": "Equipment",
+    "description": "Máy lạnh phòng điện trung+hạ thế",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "(Kiểm tra Hoạt động)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "13",
+    "comment": "Equipment",
+    "description": "Ngấm nước mương cáp điện phòng trung + hạ thế",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "(Kiểm tra Ngấm + Dột)",
+    "type": [
+    "check",
+    "Bình Thường",
+    "Có vấn đề"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "14",
+    "comment": "Equipment",
+    "description": "Cụm dầu bồi trơn BPRT",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Motor bơm dầu 1",
+    "type": [
+    "typing",
+    "Dòng điện"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Motor bơm dầu 2",
+    "type": [
+    "typing",
+    "Dòng điện"
+    ],
+    "result": { }
+    },
+    {
+    "id": "03",
+    "comment": "Content",
+    "description": "Motor bơm dầu 1",
+    "type": [
+    "typing",
+    "Nhiệt độ"
+    ],
+    "result": { }
+    },
+    {
+    "id": "04",
+    "comment": "Content",
+    "description": "Motor bơm dầu 2",
+    "type": [
+    "typing",
+    "Nhiệt độ"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "15",
+    "comment": "Equipment",
+    "description": "Cụm dầu bồi trơn D2500",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Motor bơm dầu 1",
+    "type": [
+    "typing",
+    "Dòng điện"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Motor bơm dầu 2",
+    "type": [
+    "typing",
+    "Dòng điện"
+    ],
+    "result": { }
+    },
+    {
+    "id": "03",
+    "comment": "Content",
+    "description": "Motor bơm dầu 1",
+    "type": [
+    "typing",
+    "Nhiệt độ"
+    ],
+    "result": { }
+    },
+    {
+    "id": "04",
+    "comment": "Content",
+    "description": "Motor bơm dầu 2",
+    "type": [
+    "typing",
+    "Nhiệt độ"
+    ],
+    "result": { }
+    }
+    ]
+    },
+    {
+    "id": "16",
+    "comment": "Equipment",
+    "description": "Cụm dầu Thủy Lực",
+    "array": [
+    {
+    "id": "01",
+    "comment": "Content",
+    "description": "Motor bơm dầu 1",
+    "type": [
+    "typing",
+    "Dòng điện"
+    ],
+    "result": { }
+    },
+    {
+    "id": "02",
+    "comment": "Content",
+    "description": "Motor bơm dầu 2",
+    "type": [
+    "typing",
+    "Dòng điện"
+    ],
+    "result": { }
+    },
+    {
+    "id": "03",
+    "comment": "Content",
+    "description": "Motor bơm dầu 1",
+    "type": [
+    "typing",
+    "Nhiệt độ"
+    ],
+    "result": { }
+    },
+    {
+    "id": "04",
+    "comment": "Content",
+    "description": "Motor bơm dầu 2",
+    "type": [
+    "typing",
+    "Nhiệt độ"
+    ],
+    "result": { }
+    }
+    ]
+    }
+    ]
+    },
+    {
+    "id": "02",
+    "description": "Lò gió nóng",
+    "array": [
+    {
+    "id": "01",
+    "description": "Tủ điện trung thế BPRT",
+    "array": [
+    {
+    "id": "01",
+    "description": "Relay trung thế báo lỗi",
+    "array": [ ]
+    }
+    ]
+    }
+    ]
+    },
+    {
+    "id": "03",
+    "description": "Đúc Gang",
+    "array": [
+    {
+    "id": "01",
+    "description": "Tủ điện trung thế BPRT",
+    "array": [
+    {
+    "id": "01",
+    "description": "Relay trung thế báo lỗi",
+    "array": [ ]
+    }
+    ]
+    }
+    ]
+    },
+    {
+    "id": "04",
+    "description": "Lọc Bụi gas"
+    }
+    ]
+    },
+    {
+    "id": "02",
+    "description": "Khu Vực Lò Vôi",
+    "array": [ ]
+    },
+    {
+    "id": "03",
+    "description": "Khu Vực Sinter"
+    }
+    ]
+
+const groupMenuElm = document.querySelector('.group-menu')
+const areaMenuElm = document.querySelector('.area-menu')
+const equipmentMenuElm = document.querySelector('.equipment-menu')
+const arowBackElm = document.querySelector('.arrow-back-icon')
+let currentPage = []
+let data = []
+let levelId = []
+
+// fetch('http://localhost:3000/data')
+// .then(function(result){
+//     return result.json()
+// })
+// .then(function(result){
+//     console.log(result)
+//     data = result
+//     renderGroupMenu(result)
+// })
+// .catch((err)=>{alert(err)})
+renderGroupMenu(dataMain)
+function renderGroupMenu(data){
+    var htmlRaw = data.map((crr, index)=>{
+        return `<div class="group-menu_wrap" data-index="${crr.id}">
+                    <span class="title">${crr.description}</span>
+                    <span class="detail">( 0 Warn; 0 cảnh báo; checked 230/480 )</span>
+                    <span class="present">40%</span>
+                </div>`
+    })
+    groupMenuElm.innerHTML = htmlRaw.join("")
+    menuDisplay("group")
+}
+
+
+function renderAreaMenu(input, idGroup){
+    var htmlRaw = `<h3>Không Có Dữ Liệu</h3>`
+    console.log(idGroup , input)
+    arrayTemp = input.filter((crrGroup, index)=>{
+        console.log(crrGroup.id,idGroup[0])
+        if(crrGroup.id === idGroup[0] && typeof(crrGroup.array) != "undefined"){
+            htmlRaw = (crrGroup.array.map((crrArea, index)=>{
+                return `<div class="area-menu-wrap" data-index="${crrArea.id}">
+                            <div class="area-name_wrap" >
+                                <h4 class="name">${crrArea.description}</h4>
+                                <p class="issue">( 0 Warn; 0 Error; checked 49/70 mục )</p>
+                            </div>
+                            <span class="persent">57%</span>
+                        </div>`
+            })).join("") 
+        }
+    })
+    areaMenuElm.innerHTML = htmlRaw
+    menuDisplay("area")
+}
+
+function renderEquipmentMenu(input, idArea){
+    var htmlRaw = `<h3>Không Có Dữ Liệu</h3>`
+    input.filter((crrGroup, index)=>{
+        if(crrGroup.id === idArea[0] && typeof(crrGroup.array) != "undefined"){ //Nhóm -group
+            crrGroup.array.filter((crrArea, index)=>{
+                if(crrArea.id === idArea[1] && typeof(crrArea.array) != "undefined"){ //khu vưc -Area
+                    htmlRaw = (crrArea.array.map((crrEquip, index)=>{ //Equipment
+                        return `<div class="equipment-menu-wrap item-${crrEquip.id}" data-index:"${crrEquip.id}">
+                                    <span class="name">${crrEquip.description}</span>
+                                    ${(crrEquip.array.map((crrContent, index)=>{ //content
+                                        return `<div class="wrap-content" data-index:"${crrContent.id}>
+                                                    <span class="detail">${crrContent.description}</span>
+                                                    ${contentHandel(crrContent)}   
+                                                </div>`
+                                    }
+                                    )).join("")} 
+                                </div>`
+                    })).join("")
+                }
+            })   
+        }
+                
+    })
+    equipmentMenuElm.innerHTML = htmlRaw
+    menuDisplay("equipment")
+}
+
+function contentHandel(input){
+    let html = "Error (Không tìm thấy dữ liệu)"
+    if(typeof(input.type) != "undefined")
+    {
+        switch(input.type[0]){
+            case "check":{
+                html = `<div class="check">
+                                <label class="check_name">${input.type[1]}
+                                    <input type="radio" class="check_radio" name="item-${input.id}_check-${input.id}">
+                                </label>
+                                <label class="check_name">${input.type[2]} 
+                                    <input type="radio" class="check_radio" name="item-${input.id}_check-${input.id}">
+                                </label>
+                        </div> `
+                break;
+            }
+            case "typing":{
+                let htmlTemp = ""
+                    for(let i =1; i<= input.type.length-1; i++){
+                        htmlTemp += `<label class="typing_name">${input.type[i]}
+                                        <input type="number" class="typing_input" >
+                                    </label>`
+                    }
+                html = `<div class="typing">${htmlTemp}</div> `
+                break;
+            }
+        }
+    }
+    return html
+}
+
+function menuDisplay(string){
+    switch (string) {
+        case "group":
+            {
+                groupMenuElm.style.display = "block"
+                areaMenuElm.style.display = "none"
+                equipmentMenuElm.style.display = "none"
+                currentPage= ["group"]
+            }break;
+         case "area":
+            {
+                groupMenuElm.style.display = "none"
+                areaMenuElm.style.display = "block"
+                equipmentMenuElm.style.display = "none"
+                currentPage= ["area","group"]
+            }break;
+         case "equipment":
+            {
+                groupMenuElm.style.display = "none"
+                areaMenuElm.style.display = "none"
+                equipmentMenuElm.style.display = "block"
+                currentPage= ["equipment","area","group"]
+            }break;
+    }
+        
+}
+
+function handleEven(){
+    groupMenuElm.onclick = (e)=>{
+        const idItemClick = e.target.closest('.group-menu_wrap').getAttribute("data-index")
+        levelId = [idItemClick]
+        renderAreaMenu(dataMain, levelId)
+    
+    }
+
+    arowBackElm.onclick = ()=>{
+        currentPage.length>=2? menuDisplay(currentPage[1]):""
+    }
+
+    areaMenuElm.onclick = (e)=>{
+        const idItemClick = e.target.closest('.area-menu-wrap').getAttribute("data-index")
+        var levelIdTemp = [levelId[0], idItemClick]
+        levelId = levelIdTemp
+        renderEquipmentMenu(dataMain, levelId)
+        
+    
+    }
+}
+handleEven()
